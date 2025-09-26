a55d6138eca2bc5f80ad4cd810062fe479d62d41
chromadiverse <code@chromadiverse.com>
2025-09-26 09:02:14 -0300

    cropper manually size

--- app/components/cropper.tsx
+++ app/components/cropper.tsx
@@ -18,10 +18,10 @@ interface DetectedCorners {
 
 const aspectRatios = [
   { label: 'Free', value: null },
-  { label: 'Square', value: 1 },
-  { label: 'Document', value: 4/3 },
-  { label: 'Photo', value: 3/2 },
-  { label: 'Wide', value: 16/9 },
+  { label: '1', value: 1 },
+  { label: '4/3', value: 4/3 },
+  { label: '3/2', value: 3/2 },
+  { label: '16/9', value: 16/9 },
 ]
 
 const Cropper: React.FC<CropperProps> = ({ image, onCropComplete, onBack }) => {
