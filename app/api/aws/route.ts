47874530b1ec01937b4065426d0185f9cde8874f
chromadiverse <code@chromadiverse.com>
2025-10-01 17:05:21 -0300

    route fix

--- app/api/aws/route.ts
+++ app/api/aws/route.ts
@@ -1,9 +1,9 @@
-import { NextApiResponse } from "next"
+import { NextRequest } from "next/server"
 import { randomUUID } from "crypto"
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
 
-export async function POST(req: Request, res: NextApiResponse) {
+export async function POST(req: NextRequest) {
   const { bucketName, folderName, fileType } = await req.json()
 
   const uniqueFileName = `${randomUUID()}.${fileType.split("/")[1]}`
@@ -28,6 +28,7 @@ export async function POST(req: Request, res: NextApiResponse) {
     const signedUrl = await getSignedUrl(s3Client, putCommand)
     return Response.json({ signedUrl, uniqueFileName })
   } catch (error) {
-    return Response.json({ message: "Error generating signed URL", error })
+    console.error("Error generating signed URL:", error)
+    return Response.json({ message: "Error generating signed URL", error }, { status: 500 })
   }
 }
